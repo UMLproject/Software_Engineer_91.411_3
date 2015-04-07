b6c46d113740d5a8706205c998fab90364828430
Tyler Alterio <tyalt1@gmail.com>
2015-04-06 23:54:34 -0400

    Added kinect project with basic hand tracking.

---
+++ 1_Sensor/Code/KinectSandbox/Assets/HandRightControll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandRightControll : MonoBehaviour {
+	private GameObject RightHand; // Create Variable for RightHand
+	void Start () {
+	}
+	void Update ()
+	{
+		// If Right Hand is empty null it finding else it changing hand position
+		if( RightHand == null)
+		{
+			RightHand = GameObject.Find("HandRight");
+		}
+		else
+		{
+			gameObject.transform.position = new Vector3(RightHand.transform.position.x,
+			                                            RightHand.transform.position.y,
+			                                            RightHand.transform.position.z
+			                                            );
+		}
+	}
+}
